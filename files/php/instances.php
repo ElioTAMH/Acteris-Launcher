1d4509cc01c6fe175425f7637e6532b74517a747
ElioTam <125754029+ElioTAMH@users.noreply.github.com>
2025-07-02 14:20:05 +0200

    rytr

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,5 +1,5 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
+$instance['acteris'] = array_merge($instance['acteris'], array(
     "loadder" => array(
         "minecraft_version" => "1.8.9",
         "loadder_type" => "forge",
@@ -16,31 +16,17 @@ $instance['hypixel'] = array_merge($instance['hypixel'], array(
         'shaderpacks',
         'W-OVERFLOW',
         'options.txt',
-        'optionsof.txt'
+        'optionsof.txt',
+        'servers.dat'
     ),
-    "whitelist" => array(),
-    "whitelistActive" => false,
+    "whitelist" => array(
+        'EvilerCrib27313'
+    ),
+    "whitelistActive" => true,
     "status" => array(
         "nameServer" => "Hypixel",
         "ip" => "mc.hypixel.net",
         "port" => 25565
-    )
-));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.16.5",
-        "loadder_type" => "forge",
-        "loadder_version" => "1.16.5-36.2.35"
-    ),
-    "verify" => false,
-    "ignored" => array(),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "PokeMoonX",
-        "ip" => "178.32.106.234",
-        "port" => 25599
     )
 ));
 ?>
